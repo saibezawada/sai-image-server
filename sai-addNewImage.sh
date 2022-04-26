a11b985c9290ed14f1595e44da6c01c019b3488c
Leigh Williamson <leighw@us.ibm.com>
2022-04-26 13:59:06 -0500

    add sai-addNewImage shell script

---
+++ sai-addNewImage.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+BRANCH=main
+GIT_URL=github.com/leighawilliamson/sai-image-server.git
+LOCAL_GIT_REPO=/Users/leighw/git/sai-image-server
+NEW_IMAGE=$1
+IMAGE_PROJECT=$2
+USERNAME=leighw
+PAT=JYZpyhCYowd2zVDjoYrK
+IMAGE_SERVER_URL=https://sai-image-server.n9q0ot2og27.us-south.codeengine.appdomain.cloud
+
+echo
+echo "Updating image-server project: $IMAGE_PROJECT with image file: $NEW_IMAGE..."
+if [ ! -d "$LOCAL_GIT_REPO/public/img/$IMAGE_PROJECT" ]; then
+  mkdir -p "$LOCAL_GIT_REPO/public/img/$IMAGE_PROJECT"
+fi
+cp -R "$NEW_IMAGE" "$LOCAL_GIT_REPO/public/img/$IMAGE_PROJECT"
+echo "File copied into git repo."
+cd $LOCAL_GIT_REPO
+echo "GIT STATUS before adding:"
+git status -s -b
+echo
+git add public/
+echo "GIT STATUS after adding:"
+git status -s -b
+echo
+git commit --quiet -m "Commiting new image: ${IMAGE_PROJECT}/${NEW_IMAGE}"
+git push --quiet
+#git push --quiet https://$USERNAME:$PAT@$GIT_URL $BRANCH
+echo "GIT STATUS after commit/push:"
+git status -s -b
+echo
+echo "$NEW_IMAGE has been pushed to the cloud and sai-image-server is redeploying."
+echo "Once sai-image-server finishes redeploying (~5 minutes), access this new image at:"
+echo "$IMAGE_SERVER_URL/img/${IMAGE_PROJECT}/${NEW_IMAGE}"
