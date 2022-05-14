25ea9aa0683df567b8fdd7f76634dd7397729bf9
Leigh Williamson <leighw@us.ibm.com>
2022-05-14 14:44:56 -0500

    make image server bullet-proof

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 /*eslint-env node*/
+const HOST = '0.0.0.0';
+const PORT = 8080;
 
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
@@ -7,76 +9,85 @@ const url = require('url');
 const quertstring = require('querystring');
 const bodyParser = require('body-parser');
 
-const creds = require('./image-store-creds.json');
-const cos_config = require('./cos-config.json');
-
 // create a new express server
 var app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-// serve the files out of ./public as our main files
-app.use(express.static(__dirname + '/public'));
+app.get('/',(req, res, next) => {
+  res.sendFile(__dirname + '/public/background.jpg');
+});
 
-/*
-* special route for img urls: get the image out of COS
-*/
-app.get('/img/:project/:filename', async (req, res) => {
-  var params = req.params;
-  console.log("get an image. params: ",params);
-  var result = await image_lookup(params); 
+app.get('/img/:filename', async (req, res) => {
+  console.log("get an image. params: ",req.params);
+  var result = await image_lookup(req.params); 
   res.write(result);
   res.end();
 });
 
+app.get('/img/:project/:filename', async (req, res) => {
+  console.log("get an image. params: ",req.params);
+  var result = await image_lookup(req.params); 
+  res.write(result);
+  res.end();
+});
+
+// this matches all routes and all methods i.e a centralized error handler
+app.use((req, res, next) => {
+  var fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  console.log("Invalid URL: ",fullUrl);
+  var errorText = "Invalid URL: " + fullUrl;
+  var errorMsg2 = "Valid URLs for the image-server are in the format: " + req.protocol + '://' + req.get('host') + "/img/<project>/<image-filename>";
+  res.status(404).send({
+  status: 404,
+  error: errorText,
+  errorMsg2: errorMsg2
+  })
+ });
+
+// serve the files out of ./public as our main files
+app.use(express.static(__dirname + '/public'));
+
 //start the server
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log(`server starting on http://localhost:${port}`);
+  console.log(`server listening on port: ${port}`);
 });
 
-/*
-*  image_lookup function 
-*/
+
 async function image_lookup(params){
   // establish connection to IBM Cloud Object Storage (COS)
   const COS = require('ibm-cos-sdk');
-//  let s3Options;
-  let s3Config = {
-    accessKeyId: creds.cos_hmac_keys.access_key_id,
-    secretAccessKey: creds.cos_hmac_keys.secret_access_key,
-    region: 'ibm',
-    endpoint: new COS.Endpoint(cos_config.cos_endpoint_url),
-  }
-/*
+  let s3Options;
   s3Options = {
-      accessKeyId: "a4cfad8d201548fea97b1d9a182259c6",
-      secretAccessKey: "df4620f1ea1b0fca36fc9222cb954f3d98ecbfeb06e3ed78",
+      accessKeyId: "07b3e5aaa9c640aa9e8dd8a88307c99a",
+      secretAccessKey: "ae6d93bc53524143b23e30224fad7b88778110820690f51e",
       region: 'ibm',
       endpoint: new COS.Endpoint("s3.us-east.cloud-object-storage.appdomain.cloud"),
   };
-  */
-
-  const cos = new COS.S3(s3Config);
+  const cos = new COS.S3(s3Options);
   console.log("connected to Cloud Object Storage");
-
-//  var bucket = "image-server-cos-standard-leighs-images";
-  var bucket = cos_config.cos_bucket;
-
+  var bucket = "img-server-cos-cos-standard-fza";
   var prefix = params.project;
   var filename = params.filename;
-  var fileKey = prefix + "/" + filename;
+  var fileKey;
+  if (prefix == null){
+    fileKey = filename;
+  }
+  else {
+    fileKey =   prefix + "/" + filename;
+  }
   console.log("Key: " + fileKey);
 
   let response;
   try {
     response = await cos.getObject({ Bucket: bucket, Key: fileKey }).promise();
-    console.log("retrieved file: " + prefix + "/" + filename);
+    console.log("retrieved file: " + fileKey);
   } catch (err) {
-    console.log(err);
-    throw err.message;
+    console.log(err.message);
+    var resBody = "No such file was found in the image storage: " + fileKey;
+    return resBody;
   }
-  //console.log("file contents:");
-  //console.log(response);
+  
   return response.Body;
 }
